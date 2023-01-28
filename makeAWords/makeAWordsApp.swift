2a7aad858b8ff7d2b283b55d1ff49f40c948562c
Pete <xzuvy27@gmail.com>
2023-01-28 14:37:49 +0300

    Initial Commit

---
+++ makeAWords/makeAWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  makeAWordsApp.swift
+//  makeAWords
+//
+//  Created by Peter Clarke on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct makeAWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
